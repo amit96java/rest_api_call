f1d0aec3ab2ac0d623c650c9ebf960138dcac4c8
amit96pal <amit96pal.ap@gmail.com>
2023-02-23 14:17:46 +0530

    test sum

--- src/test/java/com/amit/rest_api_call/RestApiCallApplicationTests.java
+++ src/test/java/com/amit/rest_api_call/RestApiCallApplicationTests.java
@@ -1,5 +1,6 @@
 package com.amit.rest_api_call;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -10,4 +11,8 @@ class RestApiCallApplicationTests {
 	void contextLoads() {
 	}
 
+	@Test
+	void testSum() {
+		Assertions.assertTrue(5==5);
+	}
 }
